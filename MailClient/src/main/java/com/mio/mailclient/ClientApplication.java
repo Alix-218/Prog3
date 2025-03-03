cc5eb9b01a596b24b2dfece082ed68637ec544bc
alix-218 <alice.tarquini@edu.unito.it>
2025-03-03 23:24:27 +0100

    Aggiunta la GUI del client e ottimizzato la gestione delle richieste/risposte con il server

---
+++ MailClient/src/main/java/com/mio/mailclient/ClientApplication.java
@@ -0,0 +1,32 @@
+package com.mio.mailclient;
+
+import com.mio.mailclient.controller.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class ClientApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Il mio client");
+        stage.setScene(scene);
+        //stage.setOnCloseRequest();
+
+        stage.show();
+        LoginController loginController = loader.getController();
+        loginController.setLoginStage(stage);
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        launch();
+
+    }
+}
